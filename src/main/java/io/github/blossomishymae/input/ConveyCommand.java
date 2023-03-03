ee9222ca6899bb418ffaf0d9f485385430c60e8b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-03 17:58:57 -0600

    Add command for generics

---
+++ src/main/java/io/github/blossomishymae/input/ConveyCommand.java
@@ -0,0 +1,41 @@
+package io.github.blossomishymae.input;
+
+import io.github.blossomishymae.event.SimpleEventHandler;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class ConveyCommand<T> implements IRelayCommand<T> {
+    private final Consumer<T> action;
+    private final Predicate<T> canExecutePredicate;
+    public SimpleEventHandler<Boolean> executeChanged;
+
+    public ConveyCommand(Consumer<T> action) {
+        this.action = action;
+        canExecutePredicate = null;
+        registerCanExecuteChanged(new SimpleEventHandler<>());
+    }
+
+    public ConveyCommand(Consumer<T> action, Predicate<T> canExecute) {
+        this.action = action;
+        canExecutePredicate = canExecute;
+        registerCanExecuteChanged(new SimpleEventHandler<>());
+    }
+
+    @Override
+    public boolean canExecute(T parameter) {
+        boolean canExecute = true;
+        if (canExecutePredicate != null) canExecute = canExecutePredicate.test(parameter);
+        return canExecute;
+    }
+
+    @Override
+    public void registerCanExecuteChanged(SimpleEventHandler<Boolean> canExecuteChanged) {
+        this.executeChanged = canExecuteChanged;
+    }
+
+    @Override
+    public void notifyCanExecuteChanged(Boolean canExecute) {
+        executeChanged.invoke(canExecute);
+    }
+}
